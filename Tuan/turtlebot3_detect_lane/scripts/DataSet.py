218a560ed27e1afdeaceaf9c1ec972a540554a31
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-11-09 16:46:53 +0700

    Refactor image augmentation and dataset class

--- Tuan/turtlebot3_detect_lane/scripts/DataSet.py
+++ Tuan/turtlebot3_detect_lane/scripts/DataSet.py
@@ -7,6 +7,7 @@ import os
 import random
 import math
 
+
 def augment_hsv(img, hgain=0.015, sgain=0.7, vgain=0.4):
     """change color hue, saturation, value"""
     r = np.random.uniform(-1, 1, 3) * [hgain, sgain, vgain] + 1  # random gains
@@ -18,9 +19,21 @@ def augment_hsv(img, hgain=0.015, sgain=0.7, vgain=0.4):
     lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
     lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
 
-    img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))).astype(dtype)
+    img_hsv = cv2.merge(
+        (cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))
+    ).astype(dtype)
     cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
-def random_perspective(combination,  degrees=10, translate=.1, scale=.1, shear=10, perspective=0.0, border=(0, 0)):
+
+
+def random_perspective(
+    combination,
+    degrees=10,
+    translate=0.1,
+    scale=0.1,
+    shear=10,
+    perspective=0.0,
+    border=(0, 0),
+):
     """combination of img transform"""
     # torchvision.transforms.RandomAffine(degrees=(-10, 10), translate=(.1, .1), scale=(.9, 1.1), shear=(-10, 10))
     # targets = [cls, xyxy]
@@ -53,101 +66,108 @@ def random_perspective(combination,  degrees=10, translate=.1, scale=.1, shear=1
 
     # Translation
     T = np.eye(3)
-    T[0, 2] = random.uniform(0.5 - translate, 0.5 + translate) * width  # x translation (pixels)
-    T[1, 2] = random.uniform(0.5 - translate, 0.5 + translate) * height  # y translation (pixels)
+    T[0, 2] = (
+        random.uniform(0.5 - translate, 0.5 + translate) * width
+    )  # x translation (pixels)
+    T[1, 2] = (
+        random.uniform(0.5 - translate, 0.5 + translate) * height
+    )  # y translation (pixels)
 
     # Combined rotation matrix
     M = T @ S @ R @ P @ C  # order of operations (right to left) is IMPORTANT
     if (border[0] != 0) or (border[1] != 0) or (M != np.eye(3)).any():  # image changed
         if perspective:
-            img = cv2.warpPerspective(img, M, dsize=(width, height), borderValue=(114, 114, 114))
+            img = cv2.warpPerspective(
+                img, M, dsize=(width, height), borderValue=(114, 114, 114)
+            )
             gray = cv2.warpPerspective(gray, M, dsize=(width, height), borderValue=0)
             line = cv2.warpPerspective(line, M, dsize=(width, height), borderValue=0)
         else:  # affine
-            img = cv2.warpAffine(img, M[:2], dsize=(width, height), borderValue=(114, 114, 114))
+            img = cv2.warpAffine(
+                img, M[:2], dsize=(width, height), borderValue=(114, 114, 114)
+            )
             gray = cv2.warpAffine(gray, M[:2], dsize=(width, height), borderValue=0)
             line = cv2.warpAffine(line, M[:2], dsize=(width, height), borderValue=0)
 
-
-
     combination = (img, gray, line)
     return combination
+
+
 class MyDataset(torch.utils.data.Dataset):
-    '''
+    """
     Class to load the dataset
-    '''
-    def __init__(self, transform=None,valid=False):
-        '''
+    """
+
+    def __init__(self, transform=None, valid=False):
+        """
         :param imList: image list (Note that these lists have been processed and pickled using the loadData.py)
         :param labelList: label list (Note that these lists have been processed and pickled using the loadData.py)
         :param transform: Type of transformation. SEe Transforms.py for supported transformations
-        '''
+        """
 
         self.transform = transform
         self.Tensor = transforms.ToTensor()
-        self.valid=valid
+        self.valid = valid
         if valid:
-            self.root='/home/ceec/huycq/data/bdd100k/images/val'
-            self.names=os.listdir(self.root)
+            self.root = "/home/phox/catkin_ws/src/turtlebot3_detect_lane/scripts/data/bdd100k/images/val"
+            self.names = os.listdir(self.root)
         else:
-            self.root='/home/ceec/huycq/data/bdd100k/images/train'
-            self.names=os.listdir(self.root)
+            self.root = "/home/phox/catkin_ws/src/turtlebot3_detect_lane/scripts/data/bdd100k/images/train"
+            self.names = os.listdir(self.root)
 
     def __len__(self):
         return len(self.names)
 
     def __getitem__(self, idx):
-        '''
+        """
 
         :param idx: Index of the image file
         :return: returns the image and corresponding label file.
-        '''
-        W_=640
-        H_=360
-        image_name=os.path.join(self.root,self.names[idx])
-        
+        """
+        W_ = 640
+        H_ = 360
+        image_name = os.path.join(self.root, self.names[idx])
+
         image = cv2.imread(image_name)
-        label1 = cv2.imread(image_name.replace("images","segments").replace("jpg","png"), 0)
-        label2 = cv2.imread(image_name.replace("images","lane").replace("jpg","png"), 0)
+        label1 = cv2.imread(
+            image_name.replace("images", "segments").replace("jpg", "png"), 0
+        )
+        label2 = cv2.imread(
+            image_name.replace("images", "lane").replace("jpg", "png"), 0
+        )
         if not self.valid:
-            if random.random()<0.5:
+            if random.random() < 0.5:
                 combination = (image, label1, label2)
-                (image, label1, label2)= random_perspective(
+                (image, label1, label2) = random_perspective(
                     combination=combination,
                     degrees=10,
                     translate=0.1,
                     scale=0.25,
-                    shear=0.0
+                    shear=0.0,
                 )
-            if random.random()<0.5:
+            if random.random() < 0.5:
                 augment_hsv(image)
             if random.random() < 0.5:
                 image = np.fliplr(image)
                 label1 = np.fliplr(label1)
                 label2 = np.fliplr(label2)
-            
+
         label1 = cv2.resize(label1, (W_, H_))
         label2 = cv2.resize(label2, (W_, H_))
         image = cv2.resize(image, (W_, H_))
 
-        _,seg_b1 = cv2.threshold(label1,1,255,cv2.THRESH_BINARY_INV)
-        _,seg_b2 = cv2.threshold(label2,1,255,cv2.THRESH_BINARY_INV)
-        _,seg1 = cv2.threshold(label1,1,255,cv2.THRESH_BINARY)
-        _,seg2 = cv2.threshold(label2,1,255,cv2.THRESH_BINARY)
+        _, seg_b1 = cv2.threshold(label1, 1, 255, cv2.THRESH_BINARY_INV)
+        _, seg_b2 = cv2.threshold(label2, 1, 255, cv2.THRESH_BINARY_INV)
+        _, seg1 = cv2.threshold(label1, 1, 255, cv2.THRESH_BINARY)
+        _, seg2 = cv2.threshold(label2, 1, 255, cv2.THRESH_BINARY)
 
         seg1 = self.Tensor(seg1)
         seg2 = self.Tensor(seg2)
         seg_b1 = self.Tensor(seg_b1)
         seg_b2 = self.Tensor(seg_b2)
-        seg_da = torch.stack((seg_b1[0], seg1[0]),0)
-        seg_ll = torch.stack((seg_b2[0], seg2[0]),0)
+        seg_da = torch.stack((seg_b1[0], seg1[0]), 0)
+        seg_ll = torch.stack((seg_b2[0], seg2[0]), 0)
         image = image[:, :, ::-1].transpose(2, 0, 1)
         image = np.ascontiguousarray(image)
 
-
-       
-        return image_name,torch.from_numpy(image),(seg_da,seg_ll)
-    
-
-
-
+        return image_name, torch.from_numpy(image), (seg_da, seg_ll)
