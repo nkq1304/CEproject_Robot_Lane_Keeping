b05d7d98c3967818c6ba47849f5255e6d41650b5
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-11-25 12:52:26 +0700

    Improve code readability

--- Tuan/turtlebot3_detect_lane/scripts/main.py
+++ Tuan/turtlebot3_detect_lane/scripts/main.py
@@ -3,40 +3,51 @@ import numpy as np
 import cv2
 import torch
 from model import TwinLite as net
+import math
 
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge
 
-def Run(model, img):
-    img = cv2.resize(img, (640, 360))
-    img_rs = img.copy()
-    img_rs[:,:,:]=0
+def calculate_line_equation(x1, y1, x2, y2):
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    return m, b
 
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = np.ascontiguousarray(img)
-    img = torch.from_numpy(img)
-    img = torch.unsqueeze(img, 0)  # add a batch dimension
-    img = img.cuda().float() / 255.0
-    img = img.cuda()
-    with torch.no_grad():
-        img_out = model(img)
-    x0 = img_out[0]
-    x1 = img_out[1]
+def calculate_x_intercept(m, b, y):
+    return int((y - b) / m)
 
-    _, da_predict = torch.max(x0, 1)
-    _, ll_predict = torch.max(x1, 1)
+def calculate_intersection_x(m1, b1, m2, b2):
+    return (b2 - b1) / (m1 - m2) if (m1 - m2) != 0 else float('inf')
 
-    DA = da_predict.byte().cpu().data.numpy()[0] * 255
-    LL = ll_predict.byte().cpu().data.numpy()[0] * 255
-    # img_rs[DA>100]=[255,0,0]
-    img_rs[LL > 100] = [255, 255, 255]
+def draw_in_frame(left_line, right_line, frame):
+    height, _ = frame.shape[:2]
 
-    return img_rs
+    try:
+        if left_line is not None and right_line is not None:
+            x1, y1, x2, y2 = left_line[0]
+            x3, y3, x4, y4 = right_line[0]
+            
+            m1, b1 = calculate_line_equation(x1, y1, x2, y2)
+            m2, b2 = calculate_line_equation(x3, y3, x4, y4)
 
-def detectLane(line, type_of_line):
+            line_left_color = (0, 0, 255)  # RED
+            cv2.line(frame, (calculate_x_intercept(m1, b1, height), height), (x2, y2), line_left_color, line_thickness)
+
+            line_right_color = (0, 255, 0)  # GREEN
+            cv2.line(frame, (x3, y3), (calculate_x_intercept(m2, b2, height), height), line_right_color, line_thickness)
+    except:
+        return frame
+    return frame
+
+def detect_lane(line, type_of_line):
+    global last_left_theta
+    global last_left_line
+    global last_right_theta
+    global last_right_line
+
     # Function: Detect left and right lane
-    thresh_theta = 45 #Theta in degree
+    thresh_theta = 10 # Theta in degree
     # Get theta of each line in "line"
     theta = []
     for index in range(frame_num) :
@@ -151,58 +162,28 @@ def detectLane(line, type_of_line):
         
     return line
 
-def drawInFrame(left_line, right_line, frame):
-    height, _ = frame.shape[:2]
-
-    try:
-        if left_line is not None and right_line is not None:
-            x1, y1, x2, y2 = left_line[0]
-            x3, y3, x4, y4 = right_line[0]
-            # Find linear equation of left line
-            m1 = (y2 - y1) / (x2 - x1)
-            b1 = y1 - m1 * x1
-            # Find linear equation of right line 
-            m2 = (y4 - y3) / (x4 - x3)
-            b2 = y3 - m2 * x3
-            # Draw line
-            line_left_color = (0, 0, 255) # RED
-            cv2.line(frame, (int((height - b1) / m1), height), (x2, y2), line_left_color, line_thickness) 
-            line_right_color = (0, 255, 0) # GREEN
-            cv2.line(frame, (x3, y3), (int((height - b2) / m2), height), line_right_color, line_thickness)
-    except:
-        return frame
-    return frame
-
-def identifyInvasion(left_line, right_line, frame, minDistance):
+def identify_invasion(left_line, right_line, frame, min_distance):
     _, width = frame.shape[:2]
+
     if left_line is not None and right_line is not None:
         x1, y1, x2, y2 = left_line[0]
         x3, y3, x4, y4 = right_line[0]
-        # Find linear equation of left line
-        m1 = (y2 - y1) / (x2 - x1)
-        b1 = y1 - m1 * x1
-        # Find linear equation of right line 
-        m2 = (y4 - y3) / (x4 - x3)
-        b2 = y3 - m2 * x3
-        # Find the intersection of the left and right lanes
-        inter_x = (b2 - b1) / (m1 - m2)
-        distance = inter_x - width / 2 # Variable uses to identify which lane the robot is invade on
-        if abs(distance) < minDistance:
-            return 0
-        else:
-            return distance
+
+        m1, b1 = calculate_line_equation(x1, y1, x2, y2)
+        m2, b2 = calculate_line_equation(x3, y3, x4, y4)
+
+        inter_x = calculate_intersection_x(m1, b1, m2, b2)
+        distance = inter_x - width / 2
+
+        return 0 if abs(distance) < min_distance else distance
+
+    return None
         
 def backend(binary_image):
-    global frame_count
-    global frame_array
     global right_lines
     global left_lines
-    global last_left_theta
-    global last_left_line
-    global last_right_theta
-    global last_right_line
+    global frame_array 
     global frame_num
-    global line_thickness
 
     frame_array.append(binary_image)
     gray_frame = cv2.cvtColor(binary_image, cv2.COLOR_BGR2GRAY)
@@ -235,12 +216,12 @@ def backend(binary_image):
         right_lines.append(right_line)
         left_lines.append(left_line)
 
-        right_lines = detectLane(right_lines, type_of_line = 'Right')
-        left_lines = detectLane(left_lines, type_of_line = 'Left')
+        right_lines = detect_lane(right_lines, type_of_line = 'Right')
+        left_lines = detect_lane(left_lines, type_of_line = 'Left')
 
         for l_line, r_line, _frame in zip(left_lines, right_lines, frame_array):
-            result_image = drawInFrame(l_line, r_line, _frame)
-            result_distance = identifyInvasion(l_line, r_line, _frame, minDistance = 5)
+            result_image = draw_in_frame(l_line, r_line, _frame)
+            result_distance = identify_invasion(l_line, r_line, _frame, 5)
 
         if len(right_lines) > frame_num:
             right_lines.pop(0)
@@ -253,18 +234,44 @@ def backend(binary_image):
 
         return result_image, result_distance
 
+def process_image(model, img):
+    img = cv2.resize(img, (640, 360))
+    img_rs = img.copy()
+    img_rs[:,:,:]=0
+
+    img = img[:, :, ::-1].transpose(2, 0, 1)
+    img = np.ascontiguousarray(img)
+    img = torch.from_numpy(img)
+    img = torch.unsqueeze(img, 0)  # add a batch dimension
+    img = img.cuda().float() / 255.0
+    img = img.cuda()
+    with torch.no_grad():
+        img_out = model(img)
+    x0 = img_out[0]
+    x1 = img_out[1]
+
+    _, da_predict = torch.max(x0, 1)
+    _, ll_predict = torch.max(x1, 1)
+
+    DA = da_predict.byte().cpu().data.numpy()[0] * 255
+    LL = ll_predict.byte().cpu().data.numpy()[0] * 255
+    # img_rs[DA>100]=[255,0,0]
+    img_rs[LL > 100] = [255, 255, 255]
+
+    return img_rs
+
 def image_callback(data):
     global last_angular_vel
     
     cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
     cv_image = cv2.resize(cv_image, (1280, 720))
-    binary_image = Run(model, cv_image)
+    binary_image = process_image(model, cv_image)
 
     visualized_image, distance = backend(binary_image)
 
     twist = Twist()
-    
-    if distance is not None:
+
+    if distance is not None and math.isnan(distance) is False:
         last_angular_vel = -distance * 0.01
 
     twist.angular.z = last_angular_vel
@@ -280,24 +287,21 @@ def image_callback(data):
     cv2.waitKey(1)
 
 model = net.TwinLiteNet()
-# TODO: If the model was trained with only one GPU, then comment the following line
 model = torch.nn.DataParallel(model)
 model = model.cuda()
 model.load_state_dict(torch.load("test_/model_0.pth"))
 model.eval()
 
-last_left_theta = None # Theta of last left line of previous 5 frames
-last_left_line = None # Last left line of previous 5 frames
-last_right_theta = None # Theta of last right line of previous 5 frames
-last_right_line = None # Last right line of previous 5 frames
+# Initialization of variables
+last_left_theta = None
+last_left_line = None
+last_right_theta = None
+last_right_line = None
 line_thickness = 4
-# Array of frames, right lines and left lines  
-frame_array = [] # This frame will use to write to output video
+frame_array = []  # This frame will use to write to output video
 right_lines = []
 left_lines = []
-
-frame_num = 5 # Number of frames to determine the lane
-
+frame_num = 5  # Number of frames to determine the lane
 last_angular_vel = 0
 
 bridge = CvBridge()
