81447a400e30ec6328ef1118cdd643bb5ffa52ef
khoinguyen28102002 <khoinguyennguyen914@gmail.com>
2023-11-26 13:35:42 +0700

    Improve code - Nguyen v3

--- Nguyen/detectLane/detectLane.py
+++ Nguyen/detectLane/detectLane.py
@@ -16,62 +16,81 @@ last_left_line = None # Last left line of previous 5 frames
 last_right_theta = None # Theta of last right line of previous 5 frames
 last_right_line = None # Last right line of previous 5 frames
 frame_num = 5 # Max frame's array number
-line_thickness = 4
-thresh_theta = 10 # Threshold theta in degree
+line_thickness = 4 # Thickness of 
+thresh_theta = 5 # Threshold theta in degree
+scale_thresh = 2
+frame_height = None
+frame_width = None
 
-def replaceLane(line, theta, approx_theta_arr, last_line, last_theta, error_frame):
+def findInterWithXAxis(line):
+    # global fram
+    if line is not None:
+        x1, y1, x2, y2 = line[0]
+        # Find linear equation of left line
+        m1 = (y2 - y1) / (x2 - x1)
+        b1 = y1 - m1 * x1
+        return int((frame_height - b1) / m1), frame_height
+
+def calTheta(line):
+    x1, y1, x2, y2 = line[0]
+    return np.arctan2(y2 - y1, x2 - x1)
+
+def replaceLane(line, theta, approx_theta_arr, last_line, last_theta, ef_num):
     if(last_theta == None): 
         # If these are the first 5 frames, the default are that the frames with 
         # the largest theta are approximately the same as the lane and approx_theta_arr 
         # is contain correct lanes 
-        for i in range(0, len(theta)):
-            if theta[i] not in approx_theta_arr:
-                # This's mean this is noisy line 
+        for i in range(0, len(line)):
+            if line[i] is not None:
+                theta_temp = calTheta(line[i])
+                if theta_temp not in approx_theta_arr:
+                    if i == 0:
+                        # Because this's first frame, so we need to find 
+                        # the first correct lane to replace it
+                        pos = 0
+                        for j in range (1 , len(line)):
+                            if line[j] is not None and calTheta(line[j]) in approx_theta_arr:
+                                pos = approx_theta_arr.index(calTheta(line[j]))
+                        line[i] = line[pos]
+                    else:
+                        line[i] = line[i-1]
+            else:   
                 if i == 0:
                     # Because this's first frame, so we need to find 
                     # the first correct lane to replace it
-                    pos = None
-                    for j in range (i+1 , len(theta)):
-                        pos = np.where(approx_theta_arr == theta[j])[0]
-                        if(pos.size > 0):
-                            pos = pos[0]
-                            break
+                    pos = 0
+                    for j in range (1 , len(line)):
+                        if line[j] is not None and calTheta(line[j]) in approx_theta_arr:
+                            pos = approx_theta_arr.index(calTheta(line[j]))
                     line[i] = line[pos]
-                    theta[i] = theta[pos]
                 else:
                     line[i] = line[i-1]
-                    theta[i] = theta[i-1]
-            if i == (len(theta) - 1): # Store the last line information
-                if(np.argmax(theta) is not None):
-                    last_line = line[np.argmax(theta)] 
-                    last_theta = np.max(theta)
+        last_line = line[len(line) - 1]
+        last_theta = calTheta(line[len(line) - 1])
+
     else:
-        if error_frame > int(frame_num / 2):
+        if (ef_num > int(frame_num / 2)) or (ef_num <= int(frame_num / 2) and ef_num > 0):
             # If number of error frame larger than half of number of max frame:
             # Accept these error frame are correct lane
-            for i in range (len(line) - 1, frame_num - 1, -1):
-                if line[i] is not None:                        
-                    x1, y1, x2, y2 = line[i][0]
-                    last_theta = np.arctan2(y2 - y1, x2 - x1)
-                    last_line = line[i]
-                    break
-        elif error_frame <= int(frame_num / 2) and error_frame > 0: 
-            # If number of error frame smaller than or equal to half of number of max frame
-            # Replace error lines in these error frames
-            for i in range(0, len(theta)):
-                if theta[i] not in approx_theta_arr:
-                    if i == 0:
-                        line[i] = last_line
-                        theta[i] = last_theta
+            square_angle = 90 * np.pi / 180
+            for i in range (frame_num - 1, len(line)):
+                if line[i] is not None:
+                    # print("Replace error line %s", i)
+                    x, _ = findInterWithXAxis(line[i])
+                    if x <= frame_width and x >= 0:
+                        # print('In, last line', last_line[0])
+                        theta_temp = np.abs(calTheta(line[i]))
+                        if np.bitwise_xor(np.abs(theta_temp - last_theta) > (thresh_theta * np.pi / (scale_thresh *180)),
+                                          np.abs(square_angle - theta_temp) > (thresh_theta * np.pi / (scale_thresh *180))):
+                            line[i] = last_line
                     else:
-                        line[i] = line[i-1] 
-                        theta[i] = theta[i-1] 
-                if i == (len(theta) - 1):
-                    if(np.argmax(theta) is not None):
-                        last_line = line[np.argmax(theta)] # Store the last line 
-                        last_theta = np.max(theta)
-        # The last case: number error frame equal to 0 -> all lines in all frames are correct.
-        # Not need to do any thing
+                        # print('Out, last line: ', last_line[0])
+                        line[i] = last_line
+                else: 
+                    # print('Replace not line %s', i)
+                    line[i] = last_line
+        last_line = line[len(line) - 1]
+        last_theta = calTheta(line[len(line) - 1])
     return line, last_line, last_theta
 
 def detectErrorLine(line):  
@@ -93,117 +112,52 @@ def detectErrorLine(line):
     max_theta_arr = np.sort(max(split_theta, key = len))
     # 4. Filter the retreived list again, as this list may still contain 
     # pairs deviating from each other larger than the threshold
-    aprox_theta_arr = []
+    approx_theta_arr = []
     for i in range(len(max_theta_arr)):
-        if max_theta_arr[i] - max_theta_arr[0] <= thresh_theta * np.pi / 180:
-            aprox_theta_arr.append(max_theta_arr[i])
+        if np.abs(max_theta_arr[i] - max_theta_arr[0]) <= thresh_theta * np.pi / 180:
+            approx_theta_arr.append(max_theta_arr[i])
         else:
             break
     # 5. If all theta angle are approximately with each other, return true. Otherwise, return false
-    if(len(aprox_theta_arr) == len(theta)):
-        return False, max_theta_arr, theta
+    if(len(approx_theta_arr) == len(line)):
+        return False, approx_theta_arr, theta
     else:
-        return True, max_theta_arr, theta
+        return True, approx_theta_arr, theta
     
 
 def detectLane(line, type_of_line, error_frame):
     # Function: Detect left and right lane
     global last_left_theta, last_left_line 
     global last_right_theta, last_right_line
-    # Get theta of each line in "line"
-    _, approx_theta_arr, theta = detectErrorLine(line)
+    flag, approx_theta_arr, theta = detectErrorLine(line)
+    # print(approx_theta_arr)
     if(type_of_line == 'Left'):
+        # if flag == True:
+        #     print('Replace left')
+        #     for i in range(len(line)):
+        #         if line[i] is not None:
+        #             print("Line before replace", i, line[i])
         line, last_left_line, last_left_theta = replaceLane(line, theta, approx_theta_arr, last_left_line, last_left_theta, error_frame)
-        # if(last_left_theta == None): 
-        #     # If these are the first 5 frames, the default are that the frames with 
-        #     # the largest theta are approximately the same as the lane and approx_theta_arr 
-        #     # is contain correct lanes 
-        #     for i in range(0, len(theta)):
-        #         if theta[i] not in approx_theta_arr:
-        #             # This's mean this is noisy line 
-        #             if i == 0:
-        #                 # Because this's first frame, so we need to find 
-        #                 # the first correct lane to replace it
-        #                 pos = None
-        #                 for j in range (i+1 , len(theta)):
-        #                     pos = np.where(approx_theta_arr == theta[j])[0]
-        #                     if(pos.size > 0):
-        #                         pos = pos[0]
-        #                         break
-        #                 line[i] = line[pos]
-        #                 theta[i] = theta[pos]
-        #             else:
-        #                 line[i] = line[i-1]
-        #                 theta[i] = theta[i-1]
-        #         if i == (len(theta) - 1):
-        #             if(np.argmax(theta) is not None):
-        #                 last_left_line = line[np.argmax(theta)] # Store the last line
-        #                 last_left_theta = np.max(theta)
-        # else:
-        #     if error_frame > int(frame_num / 2):
-        #         # If number of error frame larger than half of number of max frame:
-        #         # Accept these error frame are correct lane
-        #         for i in range (len(line) - 1, frame_num - 1, -1):
-        #             if line[i] is not None:                        
-        #                 x1, y1, x2, y2 = line[i][0]
-        #                 last_left_theta = np.arctan2(y2 - y1, x2 - x1)
-        #                 last_left_line = line[i]
-        #                 break
-        #     elif error_frame <= int(frame_num / 2) and error_frame > 0: 
-        #         # If number of error frame smaller than or equal to half of number of max frame
-        #         # Replace error lines in these error frames
-        #         line , last_left_line, last_left_theta = replaceLane(line, theta, approx_theta_arr, 
-        #                                                                 last_left_line, last_left_theta)
-        #     # The last case: number error frame equal to 0 -> all lines in all frames are correct.
-        #     # Not need to do any thing
+        # if flag == True:
+        #     for i in range(len(line)):
+        #         if line[i] is not None:
+        #             print('Line after replace', i, line[i])
     elif (type_of_line == 'Right'):
+        # if flag == True:
+        #     print('Replace right')
+        #     for i in range(len(line)):
+        #         if line[i] is not None:
+        #             print("Line before replace", i, line[i])
         line, last_right_line, last_right_theta = replaceLane(line, theta, approx_theta_arr, last_right_line, last_right_theta, error_frame)
-        # if(last_right_theta == None): 
-        #     # If these are the first 5 frames, the default are that the frames with 
-        #     # the largest theta are approximately the same as the lane and max_theta_array 
-        #     # is contain correct lanes 
-        #     for i in range(0, len(theta)):
-        #         if theta[i] not in approx_theta_arr:
-        #             # This's mean this is noisy line 
-        #             if i == 0:
-        #                 # Because this's first frame, so we need to find 
-        #                 # the first correct lane to replace it
-        #                 pos = None
-        #                 for j in range (i+1 , len(theta)):
-        #                     pos = np.where(approx_theta_arr == theta[j])[0]
-        #                     if(pos.size > 0):
-        #                         pos = pos[0]
-        #                         break
-        #                 line[i] = line[pos]
-        #                 theta[i] = theta[pos]
-        #             else:
-        #                 line[i] = line[i-1]
-        #                 theta[i] = theta[i-1]
-        #         if i == (len(theta) - 1):
-        #             if(np.argmax(theta) is not None):
-        #                 last_right_line = line[np.argmax(theta)] # Store the last line
-        #                 last_right_theta = np.max(theta)
-        # else:
-        #     if error_frame > int(frame_num / 2):
-        #         # If number of error frame larger than half of number of max frame:
-        #         # Accept these error frame are correct lane
-        #         for i in range (len(line) - 1, frame_num - 1, -1):
-        #             if line[i] is not None:                        
-        #                 x1, y1, x2, y2 = line[i][0]
-        #                 last_right_theta = np.arctan2(y2 - y1, x2 - x1)
-        #                 last_right_line = line[i]
-        #                 break
-        #     elif error_frame <= int(frame_num / 2) and error_frame > 0: 
-        #         # If number of error frame smaller than or equal to half of number of max frame
-        #         # Replace error lines in these error frames
-        #         line , last_right_line, last_right_theta = replaceLane(line, theta, approx_theta_arr, 
-        #                                                                 last_right_line, last_right_theta)
-        #     # The last case: number error frame equal to 0 -> all lines in all frames are correct.
-        #     # Not need to do any thing
-        
+        # if flag == True:
+        #     for i in range(len(line)):
+        #         if line[i] is not None:
+        #             print("Line after replace", i, line[i])
     return line
 
 def drawInFrame(left_line, right_line, frame):
+    # if(error_frame > 0):
+    #     print("Draw line left - right", left_line[0], right_line[0])
     height, _ = frame.shape[:2]
     if left_line is not None and right_line is not None:
         x1, y1, x2, y2 = left_line[0]
@@ -251,6 +205,7 @@ def identifyInvasion(left_line, right_line, frame, minDistance):
 def backend(video_binary, video_original):
     global last_left_theta, last_left_line 
     global last_right_theta, last_right_line
+    global frame_width, frame_height
     #Variables use to read video and write image, video
     in_img_count = 0
     out_img_count = 0
@@ -288,7 +243,6 @@ def backend(video_binary, video_original):
         image_path = os.path.join(save_in_dir, image_name)
         cv2.imwrite(image_path, frame_model)
         in_img_count += 1
-
         # Reduce noise
         gray_frame = cv2.cvtColor(frame_model, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray_frame, (5, 5), 0)
@@ -322,7 +276,9 @@ def backend(video_binary, video_original):
             # If number of error frame larger than half the number of max frame
             # or the current frame does not have any errors in lines, 
             # but the error frame count is greater than 0 frame -> Handle it
-            if (detectErrorLine(right_lines) or detectErrorLine(left_lines)):
+            is_r_lines_have_ef, _, _ = detectErrorLine(right_lines)
+            is_l_lines_have_ef, _, _ = detectErrorLine(left_lines)
+            if (is_l_lines_have_ef or is_r_lines_have_ef):
                 error_frame = error_frame + 1
                 if(error_frame > int(frame_num / 2)):
                     frame_count = frame_count + error_frame
@@ -335,12 +291,27 @@ def backend(video_binary, video_original):
         if frame_count == (frame_num + error_frame):
             # 1. Identify left and right lanes again: Because it is based on the max theta angle, 
             # error (noisy) lines may exist, so we need to remove/replace them with correct lines.
-            right_lines = detectLane(right_lines, type_of_line = 'Right', error_frame = error_frame)
-            left_lines = detectLane(left_lines, type_of_line = 'Left', error_frame = error_frame)
             if error_frame > 0:
                 end = frame_count - 1
+                # if last_left_line is not None:
+                #     print('Last left line: ', last_left_line[0])
+                #     x, _ = findInterWithXAxis(last_left_line)
+                #     if(x >= frame_width or x <= 0):
+                #         print('Last left line out')
+                #     else:
+                #         print('Last left line in')
+                # if last_right_line is not None:
+                #     print('Last right line: ', last_right_line[0])
+                #     x, _ = findInterWithXAxis(last_right_line)
+                #     if(x >= frame_width or x <= 0):
+                #         print('Last right line out')
+                #     else:
+                #         print('Last right line in')
             else:
                 end = frame_count
+            
+            right_lines = detectLane(right_lines, type_of_line = 'Right', error_frame = error_frame)
+            left_lines = detectLane(left_lines, type_of_line = 'Left', error_frame = error_frame)
 
             for l_line, r_line, _frame, i in zip(left_lines, right_lines, frame_array, range(0, end)):
                 # 2. Write this frame into output video
@@ -350,6 +321,11 @@ def backend(video_binary, video_original):
                 if (is_first_5_frame == False) or (i >= (frame_num - 1)):
                     # 1. Draw detected lines on each original frame in frame_array
                     drawInFrame(l_line, r_line, _frame)
+                    # if is_first_5_frame != False and error_frame > 0:
+                    #     x1, y1, x2, y2 = last_left_line[0]
+                    #     x3, y3, x4, y4 = last_right_line[0]            
+                    #     print('Left point: %s, %s', x2, y2)
+                    #     print('Right point: %s, %s', x3, y3)
                     # 2. Determine which lane the turtlebot is encroaching on
                     identifyInvasion(l_line, r_line, _frame, minDistance=5)
                     out.write(_frame)
@@ -364,6 +340,7 @@ def backend(video_binary, video_original):
 
             # 3. Remove some element in frame array, right lines array, left line array
             if error_frame > 0:
+                # print("Frames: %s, %s", in_img_count - error_frame, in_img_count - 1)
                 right_lines = right_lines[error_frame:]
                 left_lines = left_lines[error_frame:]
                 frame_array = frame_array[error_frame:]
@@ -374,7 +351,7 @@ def backend(video_binary, video_original):
                 left_lines = left_lines[1:]
                 frame_array = frame_array[1:]
                 frame_count = frame_count - 1
-                
+
         if cv2.waitKey(1) == ord('q'):
             break   
     output_model.release()
