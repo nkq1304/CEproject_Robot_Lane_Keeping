b4ed8a11c7eebd69dadcb99d430893241df0cd69
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2024-03-21 15:50:33 +0700

    Add CPU support

--- Tuan/lane-detector-v2/README.md
+++ Tuan/lane-detector-v2/README.md
@@ -1,5 +1,9 @@
 ## Requirement
-- Download PyTorch from [here](https://pytorch.org/get-started/locally/)
+- Download PyTorch from [here](https://pytorch.org/get-started/locally/) (GPU is recommended for better performance).
+- If your GPU not support CUDA, you can install PyTorch without CUDA by using the following command:
+```setup
+pip install torch torchvision torchaudio
+```
 - See `requirements.txt` for additional dependencies and version requirements.
 
 ```setup
--- Tuan/lane-detector-v2/lane_detector.py
+++ Tuan/lane-detector-v2/lane_detector.py
@@ -3,16 +3,26 @@ import numpy as np
 from model import TwinLite as net
 import cv2
 
+
 class LaneDetector:
     def __init__(self, config: dict) -> None:
-        self.load_model(config['model_path'])
-        self.debug = config['debug']
+        self.debug = config["debug"]
+        self.cuda = torch.cuda.is_available()
+
+        self.load_model(config["model_path"])
 
     def load_model(self, model_path: str):
         self.model = net.TwinLiteNet()
         self.model = torch.nn.DataParallel(self.model)
-        self.model = self.model.cuda()
-        self.model.load_state_dict(torch.load(model_path))
+
+        if self.cuda:
+            self.model = self.model.cuda()
+            self.model.load_state_dict(torch.load(model_path))
+        else:
+            self.model.load_state_dict(
+                torch.load(model_path, map_location=torch.device("cpu"))
+            )
+
         self.model.eval()
 
     def detect(self, img):
@@ -25,8 +35,13 @@ class LaneDetector:
         img = np.ascontiguousarray(img)
         img = torch.from_numpy(img)
         img = torch.unsqueeze(img, 0)  # add a batch dimension
-        img = img.cuda().float() / 255.0
-        img = img.cuda()
+
+        if self.cuda:
+            img = img.cuda().float() / 255.0
+            img = img.cuda()
+        else:
+            img = img.float() / 255.0
+
         with torch.no_grad():
             img_out = self.model(img)
         x0 = img_out[0]
@@ -43,6 +58,6 @@ class LaneDetector:
         if self.debug:
             visualize_img = img_copy.copy()
             visualize_img[LL > 100] = [0, 0, 255]
-            cv2.imshow('lane_detector', visualize_img)
+            cv2.imshow("lane_detector", visualize_img)
 
         return binary_img
