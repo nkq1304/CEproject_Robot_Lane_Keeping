be13974c17c3e63e4457a66fe6911f337673ec04
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2024-03-28 14:50:46 +0700

    Create turtlebot_test.py

---
+++ Tuan/lane-detector-v2/turtlebot_test.py
@@ -0,0 +1,43 @@
+import rospy
+import numpy as np
+import cv2
+import torch
+import time
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+from lane_detector import LaneDetector
+
+
+def image_callback(data):
+    frame = bridge.imgmsg_to_cv2(data, "bgr8")
+    frame = cv2.resize(frame, (640, 360))
+
+    lane_frame = lane_detector.detect(frame)
+
+    cv2.imshow("lane_frame", lane_frame)
+    cv2.waitKey(1)
+
+
+def on_shutdown():
+    print("Shutting down")
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    cfg = Config("configs/example.yaml")
+
+    lane_detector = LaneDetector(cfg.lane_detector)
+
+    bridge = CvBridge()
+
+    rospy.init_node("get_image", anonymous=True)
+    rospy.Subscriber("/camera/image", Image, image_callback)
+    rospy.on_shutdown(on_shutdown)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
